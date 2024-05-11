169df7b113a87a8c0d2a32ad46ca01073a7968df
Joel Yepez <jepes1724@gmail.com>
2024-05-11 19:36:13 -0400

    Añadiendo funcionalidad al input de crear usuario

---
+++ home/index.js
@@ -0,0 +1,38 @@
+const formCreateUser = document.querySelector('#form-create');
+const inputCreateUser = document.querySelector('#create-input');
+const notification = document.querySelector('.notification');
+const formLogin = document.querySelector('#form-login');
+const inputLogin = document.querySelector('#login-input');
+
+formCreateUser.addEventListener('submit', async e =>{
+    e.preventDefault();
+    const users = await (await fetch('http://localhost:3007/users', {method: 'GET'})).json();
+    const user = users.find(user => user.username === inputCreateUser.value);
+    if (inputCreateUser.value === '') {
+        notification.innerHTML = 'El usuario no puede estar vacio';
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 3000);
+    } else if (user) {
+        notification.innerHTML = 'El usuario ya existe';
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 3000);
+    } else {
+        await fetch('http://localhost:3007/users', {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: inputCreateUser.value}),    
+        });
+        notification.innerHTML = `El usuario ${inputCreateUser.value} ha sido creado`;
+        notification.classList.add('show-notification');
+        setTimeout(() =>{
+            notification.classList.remove('show-notification');
+        }, 3000);
+        inputCreateUser.value = '';
+    }
+})
